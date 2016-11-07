c019ebacabc291ebb9d496d35650c8be18f21b59
xander lopez <xanderlopez@me.com>
2016-11-07 03:26:41 +0000

    ho, hey!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,6 +39,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5 :)</div>
+                 <p>Ho, hey!</p>
             </div>
         </div>
     </body>
