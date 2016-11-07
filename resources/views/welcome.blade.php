44ea74273b6e9eba30caa7eb0cf5b8f0643f144a
Ram Bolista <rambolista@gmail.com>
2016-11-07 07:02:35 +0000

    added ram

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">Welcome</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page. ULOL
+                    Your Application's Landing Page. ULOL ram
                     <p>Ho, hey! :)</p>
                 </div>
             </div>
